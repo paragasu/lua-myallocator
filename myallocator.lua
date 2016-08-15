e10ac4496890862b5dd2c74a71024a7ebcc6a116
paragasu <jeffry.lunggot@trimajasa.com>
2016-08-15 13:56:39 +0800

    add api list

---
+++ myallocator.lua
@@ -0,0 +1,61 @@
+-- myallocator api client based on http://myallocator.github.io/apidocs
+-- Website: github.com/paragasu/lua-myallocator
+-- Author: Jeffry L. <paragasu@gmail.com>
+
+local request = require 'requests'
+local json = json or require 'cjson'
+local myallocator_api_url = 'https://api.myallocator.com/pms/v201408/json/'
+local auth
+
+local _M = { __VERSION = '0.01' }
+local mt = { __index = _M }
+
+-- list of supported api
+local api_method = {
+  'IRulesList',
+  'ARIRulesUpdate',
+  'ARIUpdate',
+  'ARIUpdateStatus',
+  'AssociatePropertyToPMS',
+  'AssociateUserToPMS',
+  'BookingList',
+  'BookingPaymentDownload',
+  'ChannelList',
+  'HelloUser',
+  'HelloVendor',
+  'HelloVendorUser',
+  'HelloWorld',
+  'LoopBookingCreate',
+  'PropertyChannelList',
+  'PropertyList',
+  'RoomAvailabilityList',
+  'RoomCreate',
+  'RoomList',
+  'RoomRemove',
+  'RoomUpdate',
+  'VendorSet'
+}
+
+local post_request  = function(req)
+  for k, v in pairs(auth) do req[k] = v end     
+  local res = request.post(myallocator_api_url .. method, {
+    data    = ngx.encode_args(req),
+    headers = { ['Content-Type'] = 'application/json' }
+  }) 
+
+  return res.json()
+end
+
+
+-- config table
+-- Auth/VendorId, Auth/VendorPassword, Auth/PropertyId,  Auth/UserId, Auth/UserPassword
+-- generate api method
+function _M.new(self, auth_config)
+  auth = auth_config
+  for method in api_method do 
+    _M[method] = post_request 
+  end
+  return setmetatable(_M, mt) 
+end
+
+return _M
